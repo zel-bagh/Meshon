b51f45982120371346ae56bb4e0105904489988c
Zakaria El Baghazaoui <zakaria.baghazaoui@gmail.com>
2026-02-01 22:37:49 +0100

    chore: add Prisma client singleton with postgres adapter

---
+++ src/lib/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg"
+import 'dotenv/config';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
