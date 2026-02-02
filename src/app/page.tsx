a954d37d12c036c1bfca94bddbf0bf0bfe24438f
Zakaria El Baghazaoui <zakaria.baghazaoui@gmail.com>
2026-02-02 16:45:00 +0100

    chore: verify Prisma client with simple user count

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+const users = await prisma.user.findMany();
+
   return (
     <div className="flex items-center justify-center min-h-screen min-w-screen">
       <Button>
-        hello world
+        Total Users: {users.length}
       </Button>
     </div>
   );
